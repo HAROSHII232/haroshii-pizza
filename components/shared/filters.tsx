01aaf0f6e2b22ceee77bf8214788da0638ae87f7
HAROSHII <igorgrime@gmail.com>
2025-04-20 17:25:55 +0300

    feat(filters): add RangeSlider

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,4 +1,6 @@
+import { Input } from "../ui";
 import { FilterCheckbox } from "./filter-checkbox";
+import { RangeSlider } from "./range-slider";
 import { Title } from "./title";
 
 type Props = {
@@ -14,6 +16,16 @@ export const Filters = ({ className }: Props) => {
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Новинки" value="2" />
       </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Цена от и до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={1000} defaultValue={0} />
+          <Input type="number" min={100} max={1000} placeholder="1000" />
+        </div>
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
+      </div>
+
     </div>
   );
 };
