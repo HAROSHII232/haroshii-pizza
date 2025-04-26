123ac9a03403cb4cc2420a781bdfbae94959c9b1
HAROSHII <igorgrime@gmail.com>
2025-04-26 15:36:08 +0300

    feat(header): add layout for search result block

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from "@/shared/lib";
 import { Search } from "lucide-react";
+import Link from "next/link";
 import { useRef, useState } from "react";
 import { useClickAway } from "react-use";
 
@@ -37,6 +38,25 @@ export const SearchInput = ({ className }: Props) => {
           placeholder="Найти пиццу..."
           onFocus={() => setFocused(true)}
         />
+
+        <div
+          className={cn(
+            "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
+            focused && "visible opacity-100 top-12"
+          )}
+        >
+          <Link
+            href={"/product/1"}
+            className="flex items-center gap-3 px-3 py-2 hover:bg-primary/10"
+          >
+            <img
+              className="rounded-sm h-8 w-8"
+              src="https://media.dodostatic.net/image/r:292x292/11EE7D610BBEB562BD4D48786AD87270.webp"
+              alt="pizza 1"
+            />
+            <span>Pizza 1</span>
+          </Link>
+        </div>
       </div>
     </>
   );
