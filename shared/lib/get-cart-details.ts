633f2e5565fa6f25e589cd72e3769cf8dc9ce889
HAROSHII <igorgrime@gmail.com>
2025-05-04 13:20:49 +0300

    feat(lib): add function that transforms raw cart data into a structured format

---
+++ shared/lib/get-cart-details.ts
@@ -0,0 +1,43 @@
+import { CartDTO } from "../services/dto";
+import { calcCartItemTotalPrice } from "./calc-cart-item-total-price";
+
+export type CartStateItem = {
+  id: number;
+  quantity: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  pizzaSize?: number | null;
+  pizzaType?: number | null;
+  ingredients: Array<{ name: string; price: number }>;
+};
+
+type ReturnProps = {
+  items: CartStateItem[];
+  totalAmount: number;
+};
+
+/**
+ * Transforms raw cart data into a structured format with detailed cart item information.
+ *
+ * @param cartData - The raw cart data containing items and their details.
+ * @returns An object containing a list of cart items with their details and the total cart amount.
+ */
+
+export const getCartDetails = (cartData: CartDTO): ReturnProps => {
+  const cartItems = cartData.items.map((cartItem) => ({
+    id: cartItem.id,
+    quantity: cartItem.quantity,
+    name: cartItem.productItem.product.name,
+    imageUrl: cartItem.productItem.product.imageUrl,
+    price: calcCartItemTotalPrice(cartItem),
+    pizzaSize: cartItem.productItem.size,
+    pizzaType: cartItem.productItem.pizzaType,
+    ingredients: cartItem.ingredients.map(({ name, price }) => ({ name, price })),
+  }));
+
+  return {
+    items: cartItems,
+    totalAmount: cartData.totalAmount,
+  };
+};
--- shared/services/cart.ts
+++ shared/services/cart.ts
@@ -1,9 +1,9 @@
-import { Cart } from "@prisma/client";
 import { ApiRoutes } from "./constants";
+import { CartDTO } from "./dto";
 import { axiosInstance } from "./instance";
 
-export const getCart = async (): Promise<Cart> => {
-  const { data } = await axiosInstance.get<Cart>(ApiRoutes.CART);
+export const getCart = async (): Promise<CartDTO> => {
+  const { data } = await axiosInstance.get<CartDTO>(ApiRoutes.CART);
 
   return data;
 };
