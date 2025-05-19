b928d25b393d2f8923279b24f7a2bd814f750183
HAROSHII <igorgrime@gmail.com>
2025-05-19 17:38:10 +0300

    fix(*): some fix before build

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import { Header } from "@/shared/components/shared/header";
 import { Metadata } from "next";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "HAROSHII Pizza | Главная",
@@ -15,7 +15,9 @@ export default function MainLayout({
 }>) {
   return (
     <main className="min-h-screen">
-      <Header />
+      <Suspense>
+        <Header />
+      </Suspense>
       {children}
       {modal}
     </main>
--- shared/components/ui/button.tsx
+++ shared/components/ui/button.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
-import { Loader2 } from "lucide-react";
+import { Loader } from "lucide-react";
 import { cn } from "@/shared/lib";
 
 const buttonVariants = cva(
@@ -62,7 +64,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        {!loading ? children : <Loader2 className="w-5 h-5 animate-spin" />}
+        {!loading ? children : <Loader className="w-5 h-5 animate-spin" />}
       </Comp>
     );
   }
