a23a96c710b82bf8fd47c7c94d6a4b83afa98b4e
HAROSHII <igorgrime@gmail.com>
2025-05-20 22:34:03 +0300

    fix(*): favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,6 @@ const nunito = Nunito({
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-export const metadata = {
-  icons: {
-    icon: "/favicon.ico",
-    apple: "/favicon.ico",
-  },
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -24,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link data-rh="true" rel="icon" href="/logo.png" />
+      </head>
       <body className={nunito.className}>
         <Providers>{children}</Providers>
       </body>
